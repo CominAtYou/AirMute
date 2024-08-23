f2430a85a116c0c05f3cfd091e6f7f8b87f40b07
William M <cominatyou@cominatyou.com>
2024-08-23 11:34:21 -0500

    Initial Commit

---
+++ AirMute/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  AirMute
+//
+//  Created by William Martin on 8/23/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
