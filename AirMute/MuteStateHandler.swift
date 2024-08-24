227903f862cf909b880b104401777f4ae20fcd30
William M <cominatyou@cominatyou.com>
2024-08-24 12:09:36 -0500

    Move InputMuteChangeHandler to its own file

---
+++ AirMute/MuteStateHandler.swift
@@ -0,0 +1,33 @@
+import AVFAudio
+
+extension AppDelegate {
+    func initMuteStateHandler(_ rpc: RPC) throws {
+        try AVAudioApplication.shared.setInputMuteStateChangeHandler { isMuted in
+            if self.clientInitiatedAction {
+                self.clientInitiatedAction = false
+                return true
+            }
+            
+            guard let voiceSettings = try? rpc.getVoiceSettings() else { return false }
+            
+            if voiceSettings.data.deaf {
+                if isMuted { return true }
+                
+                if !UserDefaults.standard.bool(forKey: "disable_click_to_undeafen") {
+                    do {
+                        try rpc.setMicMuted(isMuted)
+                        return true
+                    }
+                    catch { return false }
+                }
+                else { return false }
+            }
+            
+            do {
+                try rpc.setMicMuted(isMuted)
+                return true
+            }
+            catch { return false }
+        }
+    }
+}
