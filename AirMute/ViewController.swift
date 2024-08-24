eca108765759522a34b7a6386fc9389b2e8e611a
William M <cominatyou@cominatyou.com>
2024-08-24 12:08:14 -0500

    Improve settings saving behaviour, add descriptions to UI elements

--- AirMute/ViewController.swift
+++ AirMute/ViewController.swift
@@ -3,7 +3,9 @@ import Cocoa
 class ViewController: NSViewController, NSTextFieldDelegate {
     @IBOutlet weak var clientIdTextField: NSTextField!
     @IBOutlet weak var clientSecretTextField: NSTextField!
+    @IBOutlet weak var textFieldDescription: NSTextField!
     @IBOutlet weak var undeafenOnClickCheckbox: NSButton!
+    @IBOutlet weak var undeafenOnClickDescriptionLabel: NSTextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,6 +17,7 @@ class ViewController: NSViewController, NSTextFieldDelegate {
         clientIdTextField.stringValue = UserDefaults.standard.string(forKey: "client_id") ?? ""
         clientSecretTextField.stringValue = UserDefaults.standard.string(forKey: "client_secret") ?? ""
         undeafenOnClickCheckbox.state = !UserDefaults.standard.bool(forKey: "disable_click_to_undeafen") ? .on : .off
+        undeafenOnClickDescriptionLabel.stringValue = !UserDefaults.standard.bool(forKey: "disable_click_to_undeafen") ? "When deafened, clicking the stem will undeafen and unmute you." : "When deafened, clicking the stem will not do anything."
         
         undeafenOnClickCheckbox.action = #selector(checkboxStateChanged)
     }
@@ -32,7 +35,21 @@ class ViewController: NSViewController, NSTextFieldDelegate {
     }
     
     @objc func checkboxStateChanged(_ sender: NSButton) {
-        UserDefaults.standard.setValue(sender.state == .on ? false : true, forKey: "disable_click_to_undeafen")
+        if sender.state == .on {
+            undeafenOnClickDescriptionLabel.stringValue = "When deafened, clicking the stem will undeafen and unmute you."
+            
+            UserDefaults.standard.setValue(false, forKey: "disable_click_to_undeafen")
+        }
+        else {
+            undeafenOnClickDescriptionLabel.stringValue = "When deafened, clicking the stem will not do anything."
+            
+            UserDefaults.standard.setValue(true, forKey: "disable_click_to_undeafen")
+        }
+    }
+    
+    override func viewWillDisappear() {
+        UserDefaults.standard.setValue(clientIdTextField.stringValue, forKey: "client_id")
+        UserDefaults.standard.setValue(clientSecretTextField.stringValue, forKey: "client_secret")
     }
 
     override var representedObject: Any? {
