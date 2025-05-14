359a49c39843686b212da790d55bbb7dbee4f8a6
William M <cominatyou@cominatyou.com>
2025-05-14 13:08:56 -0500

    Use bundle path for launch agent

--- AirMute/SettingsViewInteractionHandlers.swift
+++ AirMute/SettingsViewInteractionHandlers.swift
@@ -1,16 +1,16 @@
 import Foundation
 
+let filePath = FileManager.default.homeDirectoryForCurrentUser.appending(path: "/Library/LaunchAgents/AirMute.plist")
+
 func launchOnStartupStateChanged(to isOn: Bool) {
     if isOn {
         let plist: NSDictionary = [
             "Label": "AirMute",
             "AssociatedBundleIdentifiers": Bundle.main.bundleIdentifier!,
-            "ProgramArguments": ["open", "/Applications/AirMute.app"],
+            "ProgramArguments": ["open", Bundle.main.bundlePath],
             "RunAtLoad": true,
             "AbandonProcessGroup": true
         ]
-        
-        let filePath = FileManager.default.homeDirectoryForCurrentUser.appending(path: "/Library/LaunchAgents/AirMute.plist")
         if FileManager.default.createFile(atPath: filePath.path(), contents: nil) {
             try? plist.write(to: filePath)
         }
